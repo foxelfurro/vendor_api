f2f0f045e64f340a0957706a728b279bbcf7d9d4
foxelfurro <93793595+foxelfurro@users.noreply.github.com>
2026-04-17 00:21:35 -0600

    Update unit price for Licencia Vendor Hub

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -140,7 +140,7 @@ export const subscribeAndCreateAccount = async (req: Request, res: Response) => 
         },
         line_items: [{
           name: "Licencia Vendor Hub",
-          unit_price: 300, 
+          unit_price: 29900, 
           quantity: 1
         }],
         charges: [{
