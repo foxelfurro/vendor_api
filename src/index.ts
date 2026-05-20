f9897ac188aa59a66379953d04c41c5c546f62d4
foxelfurro <93793595+foxelfurro@users.noreply.github.com>
2026-05-19 22:41:36 -0600

    Refactor imports and add logout route

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,24 @@
 import express from 'express';
 import cors from 'cors';
-import cookieParser from 'cookie-parser'; 
+import cookieParser from 'cookie-parser'; // 1. Importación hasta arriba
 
 import { verifyToken, isAdmin } from './middlewares/auth.middleware';
+// 2. Importamos la función de logout aquí
 import { login, logout, getMe, subscribeAndCreateAccount, forgotPassword, resetPassword, renewSubscription} from './controllers/auth.controller';
 import { getSalesHistory, registerSale } from './controllers/sales.controller';
-
-// 1. IMPORTACIONES UNIFICADAS DEL VENDEDOR (Todo en una sola línea)
-import { exploreCatalog, getInventory, addToInventory, updateInventoryStock, addCustomToInventory, getSellerCatalogBySlug, updateStoreSettings } from './controllers/vendor.controller';
-
+import { exploreCatalog, getInventory, addToInventory, updateInventoryStock, getSellerCatalogBySlug, updateStoreSettings, requestCatalogItem } from './controllers/vendor.controller';
 import { getDashboardStats } from './controllers/dashboard.controller';
 import { createUser, createCatalogItem } from './controllers/admin.controller';
 
+
 const app = express();
 
-// Activamos el parseo de cookies primero
+
+
+// 3. Activamos el parseo de cookies primero
 app.use(cookieParser());
 
-// Configuramos el CORS
+// 4. Configuramos el CORS una sola vez con el dominio base
 app.use(cors({
     origin: ['https://lumin.qlatte.com', 'http://localhost:5173','https://api.qlatte.com' ],
     credentials: true 
@@ -27,7 +28,7 @@ app.use(express.json());
 
 // --- RUTAS PÚBLICAS (No requieren token) ---
 app.post('/auth/login', login);
-app.post('/auth/logout', logout); 
+app.post('/auth/logout', logout); // <-- Añadimos la ruta de logout aquí
 app.post('/auth/forgot-password', forgotPassword);
 app.post('/auth/reset-password', resetPassword);
 app.post('/auth/subscribe', subscribeAndCreateAccount); 
@@ -42,17 +43,13 @@ app.post('/admin/catalogo', verifyToken, isAdmin, createCatalogItem);
 // Operaciones de Vendedor
 app.get('/vendor/explore', verifyToken, exploreCatalog);
 app.get('/vendor/inventory', verifyToken, getInventory);
-
-
-app.post('/vendor/inventory/custom', verifyToken, addCustomToInventory);
-
 app.post('/vendor/inventory', verifyToken, addToInventory);
 app.put('/vendor/inventory/:id', verifyToken, updateInventoryStock);
 app.get('/vendor/dashboard-stats', verifyToken, getDashboardStats);
 app.put('/vendor/store-settings', verifyToken, updateStoreSettings);
-
+app.post('/vendor/request-catalog', verifyToken, requestCatalogItem);
 // Ventas y Registro
-app.post('/sales/register', verifyToken, registerSale); 
+app.post('/sales/register', verifyToken, registerSale); // Venta local
 app.get('/sales/history', verifyToken, getSalesHistory);
 
 // Perfil y Autenticación
